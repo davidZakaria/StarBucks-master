aa652630017ee41fe42fa0db7d054474b1bb7cea
davidZakaria <david150778@bue.edu.eg>
2022-06-04 16:58:14 +0200

    ..

---
+++ models/drinks.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+// const Joi = require('joi');
+// Joi.objectId = require('joi-objectid')(Joi);
+// const {validatedrinks, drinksSchema} = require('./drinks');
+
+const Drinks = mongoose.model('Drinks', new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 55,
+  },
+  Hotorcold: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+  price: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+  },
+}));
+
+module.exports = Drinks;
+
+
---
+++ models/hooka.js
@@ -0,0 +1,17 @@
+// const { Sequelize, DataTypes } = require("sequelize");
+
+// const Hookah = sequelize.define("hookah", {
+//   id: {
+//     type: Sequelize.DataTypes.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//   },
+//   Flavour: {
+//     type: Sequelize.DataTypes.STRING,
+//   },
+//   Price: {
+//     type: Sequelize.DataTypes.INTEGER,
+//   },
+// });
+
+// module.exports = Hookah;
