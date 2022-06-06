c4b574034033cfa96ab6e3fddadfb8efc955843c
The-Naty <youssef-el-zanaty@outlook.com>
2022-06-06 02:00:07 +0200

    N002

--- models/food.js
+++ models/food.js
@@ -1,27 +1,27 @@
 const mongoose = require("mongoose");
 
-const Food = mongoose.model('Food', new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 55,
-  },
-  type: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 255,
-  },
-  price: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 255,
-  },
-}));
+const Food = mongoose.model(
+  "Food",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 55,
+    },
+    type: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+    },
+    price: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+    },
+  })
+);
 
 module.exports = Food;
-
-// module.exports.Food = foodSchema;
-
