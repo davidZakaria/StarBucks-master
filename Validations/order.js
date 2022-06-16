cf5468f2e0c6c2901025b6a1979ee18a0d5bf2ae
The-Naty <youssef-el-zanaty@outlook.com>
2022-06-16 19:25:11 +0200

    update

--- Validations/order.js
+++ Validations/order.js
@@ -3,10 +3,10 @@ const Joi = require("joi");
 const createOrder = {
   body: Joi.object()
     .keys({
-      orderNo: Joi.number().required(),
-      userId: Joi.number().required(),
-      date: Joi.date().required(),
-      totalPrice: Joi.number().required(),
+      orderNo: Joi.number(),
+      userId: Joi.number(),
+      date: Joi.date(),
+      totalPrice: Joi.number(),
       items: Joi.array().required(),
     })
     .required(),
